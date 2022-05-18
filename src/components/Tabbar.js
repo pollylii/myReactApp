fc057cc3d3887711cd987b407004adb36bade306
pollylilh <794211268@qq.com>
2022-05-18 14:04:39 +0800

    css模块化

--- src/components/Tabbar.js
+++ src/components/Tabbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import '../style/tabbar.css'
+import style from '../style/tabbar.module.css'
 
 
 export default function Tabbar() {
   return (
-    <div>
+    <div className={style.tab + ' aaa'}>
       <ul>
         <li>
-          <NavLink to='/films' activeClassName='tabActive'>电影</NavLink>
+          <NavLink to='/films' activeClassName={style.tabActive}>电影</NavLink>
         </li>
         <li>
-          <NavLink to='/cinemas' activeClassName='tabActive'>影院</NavLink>
+          <NavLink to='/cinemas' activeClassName={style.tabActive}>影院</NavLink>
         </li>
         <li>
-          <NavLink to='/center' activeClassName='tabActive'>我的</NavLink>
+          <NavLink to='/center'activeClassName={style.tabActive}>我的</NavLink>
         </li>
       </ul>
     </div>
--- src/views/films/ComingSoon.js
+++ src/views/films/ComingSoon.js
@@ -2,16 +2,43 @@ import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class ComingSoon extends Component {
-  componentDidMount() {
+  state = {
+    filmList: []
+  }
+  
+  getFilmList = () => {
+    // optimus_uuid 猫眼电影设置了时效？次数
+    //ajax/comingList?ci=30&limit=10&movieIds=&token=&optimus_uuid=EF69D7C0D65811ECA16AAB6147C9C886345E039BEC114EAC91EE9B6EACC90DFA&optimus_risk_level=71&optimus_code=10
     axios.get('/ajax/comingList?ci=30&limit=10&movieIds=&token=&optimus_uuid=161F2CB0D5ED11EC9FE64975ACF4C9B5A83AD0E75CBE4ECEB673B77CD7BCAEA9&optimus_risk_level=71&optimus_code=10').then(res => {
-      console.log(9999, res);
+      this.setState({
+        filmList: res.data.coming
+      })
     })
   }
+
+  componentDidMount() {
+    this.getFilmList()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.location.pathname === prevProps.location.pathname) {
+      return
+    }
+  }
+
   render() {
+    let list = this.state.filmList ? this.state.filmList : [{id: 1,nm: 'optimus_uuid时效已过'}]
     return (
-      <div>ComingSoon</div>
+      <div>
+        <ul>
+          {
+            list.map(item =>
+              <li key={item.id}>{item.nm}</li>
+            )
+          }
+        </ul>
+      </div>
     )
   }
 }
 
-// https://i.maoyan.com/ajax/comingList?ci=30&limit=10&movieIds=&token=&optimus_uuid=161F2CB0D5ED11EC9FE64975ACF4C9B5A83AD0E75CBE4ECEB673B77CD7BCAEA9&optimus_risk_level=71&optimus_code=10
